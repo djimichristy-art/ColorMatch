c3f7471adf30a4875bb1bffc59ff50b5db0eb3ca
djimichristy-art <djimichristy@gmail.com>
2026-02-26 10:13:13 +0100

    changed the script to make the game more difficult

--- color_match.gd
+++ color_match.gd
@@ -3,18 +3,19 @@ extends Control
 # Words and their colors
 var words = ["RED", "BLUE", "GREEN", "YELLOW", "PURPLE", "ORANGE", "WHITE", "BLACK"]
 var colors = [
-	Color.RED,       # RED
-	Color.BLUE,      # BLUE
-	Color.GREEN,     # GREEN
-	Color.YELLOW,    # YELLOW
-	Color(0.56,0,1), # PURPLE
-	Color(1,0.5,0),  # ORANGE
-	Color.WHITE,     # WHITE
-	Color.BLACK      # BLACK
+	Color.RED,
+	Color.BLUE,
+	Color.GREEN,
+	Color.YELLOW,
+	Color(0.56,0,1), # Purple
+	Color(1,0.5,0),  # Orange
+	Color.WHITE,
+	Color.BLACK
 ]
 
 # Game variables
 var score := 0
+var combo := 0
 var current_color : Color
 var game_over := false
 var time_left := 25
@@ -25,6 +26,8 @@ var time_left := 25
 @onready var timer_label = $TimerLabel
 @onready var background = $Background
 @onready var restart_btn = $ButtonContainer/RestartButton
+@onready var game_timer = $GameTimer
+
 @onready var red_btn = $ButtonContainer/RedButton
 @onready var blue_btn = $ButtonContainer/BlueButton
 @onready var green_btn = $ButtonContainer/GreenButton
@@ -33,28 +36,22 @@ var time_left := 25
 @onready var orange_btn = $ButtonContainer/OrangeButton
 @onready var white_btn = $ButtonContainer/WhiteButton
 @onready var black_btn = $ButtonContainer/BlackButton
-@onready var game_timer = $GameTimer
+
 
 func _ready():
 	randomize()
 
-	# Reset UI
+	score = 0
+	combo = 0
+	time_left = 25
+	game_over = false
+
 	score_label.text = "Score: 0"
-	timer_label.text = "Time: " + str(time_left)
-	word_label.text = ""
+	timer_label.text = "Time: 25"
 	restart_btn.visible = false
+	background.color = Color(1, 0.75, 0.8)
 
-	# Set button colors
-	red_btn.self_modulate = Color.RED
-	blue_btn.self_modulate = Color.BLUE
-	green_btn.self_modulate = Color.GREEN
-	yellow_btn.self_modulate = Color.YELLOW
-	purple_btn.self_modulate = Color(0.56,0,1)
-	orange_btn.self_modulate = Color(1,0.5,0)
-	white_btn.self_modulate = Color.WHITE
-	black_btn.self_modulate = Color.BLACK
-
-	# Connect buttons
+	# CONNECT ALL COLOR BUTTONS
 	red_btn.pressed.connect(func(): _on_color_pressed(Color.RED))
 	blue_btn.pressed.connect(func(): _on_color_pressed(Color.BLUE))
 	green_btn.pressed.connect(func(): _on_color_pressed(Color.GREEN))
@@ -64,11 +61,11 @@ func _ready():
 	white_btn.pressed.connect(func(): _on_color_pressed(Color.WHITE))
 	black_btn.pressed.connect(func(): _on_color_pressed(Color.BLACK))
 
+	# CONNECT RESTART BUTTON
 	restart_btn.pressed.connect(_on_restart_pressed)
 
-	# Setup timer
+	# TIMER SETUP
 	game_timer.wait_time = 1
-	game_timer.one_shot = false
 	game_timer.timeout.connect(_on_timer_timeout)
 	game_timer.start()
 
@@ -79,14 +76,11 @@ func show_new_word():
 	if game_over:
 		return
 
-	# Background stays pink
-	background.color = Color(1, 0.75, 0.8)
+	var word_index = randi() % words.size()
+	var color_index = randi() % colors.size()
 
-	# Random word
-	word_label.text = words[randi() % words.size()]
-
-	# Random word color
-	current_color = colors[randi() % colors.size()]
+	word_label.text = words[word_index]
+	current_color = colors[color_index]
 	word_label.add_theme_color_override("font_color", current_color)
 
 
@@ -95,13 +89,16 @@ func _on_color_pressed(chosen_color: Color):
 		return
 
 	if chosen_color == current_color:
-		score += 1
+		combo += 1
+		score += 1 + combo
+		time_left = max(time_left - 1, 5) # reduce time but not below 5
 	else:
+		combo = 0
 		score = max(score - 1, 0)
 
 	score_label.text = "Score: " + str(score)
+	timer_label.text = "Time: " + str(time_left)
 
-	await get_tree().create_timer(0.4).timeout
 	show_new_word()
 
 
@@ -112,21 +109,29 @@ func _on_timer_timeout():
 	time_left -= 1
 	timer_label.text = "Time: " + str(time_left)
 
+	# Low time warning
+	if time_left <= 5:
+		background.color = Color(1, 0.4, 0.4)
+	else:
+		background.color = Color(1, 0.75, 0.8)
+
 	if time_left <= 0:
 		game_over = true
-		word_label.text = "GAME OVER"
-		restart_btn.visible = true
 		game_timer.stop()
+		word_label.text = "GAME OVER\nSCORE: " + str(score)
+		restart_btn.visible = true
 
 
 func _on_restart_pressed():
 	score = 0
+	combo = 0
 	time_left = 25
 	game_over = false
 
 	score_label.text = "Score: 0"
-	timer_label.text = "Time: " + str(time_left)
+	timer_label.text = "Time: 25"
 	restart_btn.visible = false
+	background.color = Color(1, 0.75, 0.8)
 
 	game_timer.start()
 	show_new_word()
