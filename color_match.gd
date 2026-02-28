c7b34d41d042d5fd70afea140a9f8fa0a8cf15cc
djimichristy-art <djimichristy@gmail.com>
2026-02-28 19:22:40 +0100

    edited the start and end pages

--- color_match.gd
+++ color_match.gd
@@ -1,100 +1,111 @@
-
 extends Control
 
-# --- Constants ---
-var COLORS = ["RED", "BLUE", "GREEN", "YELLOW", "PURPLE", "ORANGE", "WHITE", "BLACK"]
-var COLOR_VALUES = {
-	"RED": Color(1,0,0),
-	"BLUE": Color(0,0,1),
-	"GREEN": Color(0,1,0),
-	"YELLOW": Color(1,1,0),
-	"PURPLE": Color(0.5,0,0.5),
-	"ORANGE": Color(1,0.5,0),
-	"WHITE": Color(1,1,1),
-	"BLACK": Color(0,0,0)
-}
+# Words and their display colors
+var words = ["RED", "BLUE", "GREEN", "YELLOW"]
+var colors = [Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW]
 
-# --- Variables ---
-var score: int = 0
-var time_left: int = 20
-var current_color_name: String = ""
-var bg_timer_counter: float = 0
+# Score and timer
+var score = 0
+var time_left = 20  # 20 seconds
 
-# --- Nodes ---
-@onready var word_label = $WordLabel
+# UI nodes
 @onready var score_label = $ScoreLabel
+@onready var word_label = $WordLabel
 @onready var timer_label = $TimerLabel
-@onready var timer = $GameTimer
-@onready var buttons_container = $ButtonsContainer
-@onready var background = $Background  # ColorRect
+@onready var red_button = $RedButton
+@onready var blue_button = $BlueButton
+@onready var green_button = $GreenButton
+@onready var yellow_button = $YellowButton
+@onready var game_timer = $GameTimer        # 1-second tick timer
+@onready var background_timer = $BGTimer    # 3-second tick timer
+@onready var root_control = self            # root node for changing background
 
-# --- Ready ---
+# Current correct color index
+var current_color_index = 0
+
 func _ready():
-	randomize()
-	score = 0
-	time_left = 20
-	update_score_label()
-	update_timer_label()
-	pick_new_color()
-	
+	randomize()  # randomize words/colors
+
+	# Set button colors
+	red_button.add_color_override("font_color", Color.white)
+	red_button.add_color_override("font_color_pressed", Color.white)
+	red_button.add_color_override("font_color_hover", Color.white)
+	red_button.add_color_override("font_color_disabled", Color.gray)
+	red_button.add_color_override("bg_color", Color.red)
+
+	blue_button.add_color_override("font_color", Color.white)
+	blue_button.add_color_override("bg_color", Color.blue)
+
+	green_button.add_color_override("font_color", Color.white)
+	green_button.add_color_override("bg_color", Color.green)
+
+	yellow_button.add_color_override("font_color", Color.black)
+	yellow_button.add_color_override("bg_color", Color.yellow)
+
 	# Connect buttons
-	if buttons_container:
-		for button in buttons_container.get_children():
-			var color_name = button.name.replace("Button","").to_upper()
-			button.set_meta("COLOR_NAME", color_name)
-			if COLOR_VALUES.has(color_name):
-				button.modulate = COLOR_VALUES[color_name]
-			if not button.pressed.is_connected(self._on_answer_button_pressed):
-				button.pressed.connect(_on_answer_button_pressed, [button])
-	
-	if timer:
-		timer.start()
-	else:
-		print("Timer node not found!")
+	red_button.pressed.connect(self.check_answer.bind(0))
+	blue_button.pressed.connect(self.check_answer.bind(1))
+	green_button.pressed.connect(self.check_answer.bind(2))
+	yellow_button.pressed.connect(self.check_answer.bind(3))
 
-# --- Pick new color ---
-func pick_new_color():
-	if COLORS.size() == 0: return
-	current_color_name = COLORS[randi() % COLORS.size()]
-	if word_label:
-		word_label.text = current_color_name
+	# Setup game timer (1 second ticks)
+	game_timer.wait_time = 1
+	game_timer.one_shot = false
+	game_timer.start()
+	game_timer.timeout.connect(_on_timer_tick)
 
-# --- Button pressed ---
-func _on_answer_button_pressed(button):
-	if button == null: return
-	var button_color = button.get_meta("COLOR_NAME")
-	if button_color == current_color_name:
-		score += 1
-	update_score_label()
-	pick_new_color()
+	# Setup background timer (3 seconds ticks)
+	background_timer.wait_time = 3
+	background_timer.one_shot = false
+	background_timer.start()
+	background_timer.timeout.connect(_on_background_change)
 
-# --- Labels ---
-func update_score_label():
-	if score_label:
-		score_label.text = "Score: " + str(score)
+	# Initialize UI
+	update_score()
+	update_timer()
+	show_new_word()
 
-func update_timer_label():
-	if timer_label:
-		timer_label.text = "Time: " + str(time_left)
-
-# --- Background color change ---
-func _process(delta):
-	bg_timer_counter += delta
-	if bg_timer_counter >= 3.0:
-		bg_timer_counter = 0
-		if background:
-			# Lighter random colors so text is readable
-			background.color = Color(randf()*0.6 + 0.4, randf()*0.6 + 0.4, randf()*0.6 + 0.4)
-
-# --- Timer timeout ---
-func _on_GameTimer_timeout():
-	time_left -= 1
-	update_timer_label()
+func show_new_word():
 	if time_left <= 0:
-		if timer:
-			timer.stop()
-		var end_page = get_parent().get_node("EndPage")
-		if end_page:
-			end_page.show_score(score)
-			self.visible = false
-			end_page.visible = true
+		return
+	var word_index = randi() % words.size()
+	current_color_index = randi() % colors.size()
+	word_label.text = words[word_index]
+	word_label.add_color_override("font_color", colors[current_color_index])
+
+func check_answer(button_index):
+	if time_left <= 0:
+		return  # game over
+	if button_index == current_color_index:
+		score += 1
+	else:
+		if score > 0:
+			score -= 1
+	update_score()
+	show_new_word()
+
+func update_score():
+	score_label.text = "Score: %d" % score
+
+func update_timer():
+	timer_label.text = "Time: %d" % time_left
+
+func _on_timer_tick():
+	if time_left > 0:
+		time_left -= 1
+		update_timer()
+	else:
+		game_over()
+
+func _on_background_change():
+	# Randomly change background color
+	root_control.add_color_override("custom_colors/bg_color", colors[randi() % colors.size()])
+
+func game_over():
+	game_timer.stop()
+	background_timer.stop()
+	word_label.text = "Game Over!"
+	red_button.disabled = true
+	blue_button.disabled = true
+	green_button.disabled = true
+	yellow_button.disabled = true
--- startpage.gd
+++ startpage.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var start_button = $StartButton
-@onready var start_audio = $StartPageAudio  # optional, if you have a sound
+@onready var start_audio = $StartPageAudio
 
 func _ready():
 	start_button.pressed.connect(_on_start_pressed)
@@ -9,5 +9,8 @@ func _ready():
 func _on_start_pressed():
 	if start_audio:
 		start_audio.play()
-	# Load the ColorMatchGame scene
-	get_tree().change_scene_to_file("res://ColorMatchGame.tscn")  # Make sure this path matches your file
+	get_tree().change_scene_to_file("res://color_match.tscn")
+
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
